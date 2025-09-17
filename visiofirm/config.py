a074fbb443e783adcf318d7c1bbbd8995ea8e81d
safouane95 <safouane.elghazouali@gmail.com>
2025-09-17 16:16:48 +0200

    adding support for classifcation and video formats

--- visiofirm/config.py
+++ visiofirm/config.py
@@ -18,5 +18,17 @@ def get_db_path():
     return os.path.join(get_cache_folder(), 'users.db')
 
 PROJECTS_FOLDER = get_cache_folder()
+
 VALID_IMAGE_EXTENSIONS = {'.webp', '.jpg', '.jpeg', '.JPG', '.JPEG', '.png', '.avif'}
+
 WEIGHTS_FOLDER = os.path.join(get_cache_folder(), 'weights')
+
+TMP_FOLDER = os.path.join(get_cache_folder(), 'tmp')
+
+VALID_VIDEO_EXTENSIONS = {'.mp4', '.avi', '.mov', '.mkv'}
+
+VALID_SETUP_TYPES = {"Bounding Box", "Oriented Bounding Box", "Segmentation", "Classification", "Video Detection", "Video Segmentation"}
+
+VALID_IMAGE_FORMATS = {'COCO', 'YOLO', 'PASCAL_VOC', 'CSV', 'MASK'}
+
+VALID_VIDEO_FORMATS = {'COCO_VIDEO', 'MOT', 'COCO_SEG_VIDEO', 'MASK_SEQUENCE', 'MASK_VIDEO'}
