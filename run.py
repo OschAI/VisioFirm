06c3b4063c86c8f4d05446e3f4d723126e67fd6e
safouane95 <safouane.elghazouali@gmail.com>
2025-09-17 16:10:36 +0200

    VF launcher update

--- run.py
+++ run.py
@@ -1,11 +1,13 @@
 import socket
 import threading
 import webbrowser
-import waitress 
-from visiofirm import create_app
-import multiprocessing
+import sys
+import visiofirm
+import atexit  
+from visiofirm.errortracker import VFSessionTracker 
+import uvicorn 
 
-app = create_app()
+app = visiofirm.create_app()
 
 def find_free_port(start_port=8000):
     port = start_port
@@ -18,6 +20,11 @@ def find_free_port(start_port=8000):
                 port += 1 
 
 def main():
+    # Handle version flags
+    if any(arg in ('-V', '--version') for arg in sys.argv[1:]):
+        print(f"VisioFirm v{visiofirm.__version__}")
+        return
+
     port = find_free_port()
     url = f"http://localhost:{port}"
     
@@ -30,16 +37,28 @@ WELCOME TO
    \ \/ / | / __| |/ _ \|  __| | | '__| '_ ` _ \ 
     \  /  | \__ \ | (_) | |    | | |  | | | | | |
      \/   |_|___/_|\___/|_|    |_|_|  |_| |_| |_|                          
- 
-You are currently running the version:
-VisioFirm v0.2.0
 
+You are currently running the version:
+VisioFirm v{}
+
+VisioFirm running in {}
 Stay updated by visiting our GitHub Repository https://github.com/OschAI/VisioFirm
-""".format(port=port))
+""".format(visiofirm.__version__, url))
+    
+    tracker = VFSessionTracker()
+    tracker.start_session() 
+    app.tracker = tracker 
+    
+    def shutdown_tracker():
+        tracker.end_session(print_path=True) 
+    atexit.register(shutdown_tracker)
     
     threading.Timer(1.5, lambda: webbrowser.open(url)).start()
-    threads = max(4, multiprocessing.cpu_count() * 2) 
-    waitress.serve(app, host="localhost", port=port, threads=threads)
+    
+    # threads = max(4, multiprocessing.cpu_count() * 2)
+    config = uvicorn.Config(app=app, host="localhost", port=port, access_log=False, log_level="info", reload=False)
+    server = uvicorn.Server(config)
+    server.run()
 
 if __name__ == "__main__":
     main()
